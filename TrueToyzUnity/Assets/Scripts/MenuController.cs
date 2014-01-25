f4a6bae78e408cf89dee3bed3f1a2bb2ba4b61ee
Cecilia Lejeune <cecilia.lejeu@gmail.com>
2014-01-25 16:04:04 +0100

    Added the menuController script to switch between Play and Quit in menu

---
+++ TrueToyzUnity/Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public int radius = 10;
+
+	private GameObject quitButton;
+	private GameObject playButton;
+
+	void Awake () {
+		quitButton = GameObject.FindGameObjectWithTag("Quit");
+		playButton = GameObject.FindGameObjectWithTag("Play");
+	}
+
+	void Update () {
+		RaycastHit hit;
+
+		if (Physics.Raycast(AvatarManager.vrHeadNode.transform.position, transform.forward, out hit, radius)){
+
+			if ( hit.collider.gameObject == playButton) {
+
+				//Load the main scene
+				Debug.Log ("PLAY ! " );
+				Application.LoadLevel("Sandbox");
+			}
+			else if ( hit.collider.gameObject == quitButton) {
+
+				//Quit the game
+				Application.Quit();
+			}
+
+		}
+	}
+}
