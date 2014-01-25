ca4221cbc0e65ec60e685bed2e4c37de2e025f06
Kevin Wagrez <kevin.wagrez@gmail.com>
2014-01-25 01:04:11 +0100

    First commit, MiddleVR included, and a meaningless single script just for show.
    Signed-off-by: Kevin Wagrez <kevin.wagrez@gmail.com>

---
+++ TrueToyzUnity/Assets/Scripts/ChildBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildBehaviour : MonoBehaviour {
+
+	public GameObject m_ToyInHand; // Toy actually hold in hand
+	
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+
+	/* ------------------------------------ Control functions --------------------------- */
+
+	/*
+	 * Take control of child, moving VR root hierarchy to se through his eyes and contro l his hand
+	 * */
+	void TakeControl ()
+	{
+
+	}
+
+	/*
+	 * Release control of child
+	 * Automatically release toy if in hand
+	 * Pause Child ?
+	 * */
+	void ReleaseControl () 
+	{
+
+	}
+
+	/* --------------------------------------- Interaction functions --------------------------- */
+
+
+	/* ------------------------------------------ Menu functions ---------------------------------- */
+
+	/*
+	 * Pause child when menu is On
+	 * */
+	void Pause ()
+	{
+
+	}
+}
---
+++ TrueToyzUnity/Assets/Scripts/ToyPlayerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToyPlayerBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+}
