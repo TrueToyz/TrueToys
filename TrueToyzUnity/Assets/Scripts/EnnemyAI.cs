8c76200026b95bc6a7e046b817f7725cad0d614b
Cecilia Lejeune <cecilia.lejeu@gmail.com>
2014-01-25 03:37:47 +0100

    Linked two scripts with the enemy for it's basic chasing behaviour

---
+++ TrueToyzUnity/Assets/Scripts/EnnemyAI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyAI : MonoBehaviour {
+
+	public float chaseSpeed = 2.0f;
+	public float chaseWaitTime = 2f;
+
+	private EnnemySight ennemySight;
+	private LastPlayerSighting lastPlayerSighting;
+	private GameObject player;
+	private float chaseTimer;
+	
+	void Awake () {
+
+		player = GameObject.FindGameObjectWithTag("ChildToy");
+		ennemySight = GetComponent<EnnemySight>();
+		lastPlayerSighting = GameObject.FindGameObjectWithTag("GameController").GetComponent<LastPlayerSighting>();
+	}
+
+	void Update () {
+		if (ennemySight.playerInSight) {
+			Chasing ();
+		}
+	}
+
+	void Chasing () {
+	
+		Vector3 sightingDeltaPos = ennemySight.personalLastSighting - transform.position;
+
+		// If the the last personal sighting of the player is not close...
+		if(sightingDeltaPos.sqrMagnitude > 4f) {
+			Debug.Log("Chasing !");
+
+			Vector3 destination = player.transform.position - transform.position ;
+			transform.Translate(destination.normalized * chaseSpeed * Time.deltaTime);
+
+			// Timer
+			chaseTimer += Time.deltaTime;
+
+			if(chaseTimer >= chaseWaitTime)
+			{
+				Debug.Log(" ------------ TIME OUT : quit chasing -------------- ");
+				lastPlayerSighting.position = lastPlayerSighting.resetPosition;
+				ennemySight.personalLastSighting = lastPlayerSighting.resetPosition;
+				chaseTimer = 0f;
+			}
+		}
+
+
+	}
+
+}
---
+++ TrueToyzUnity/Assets/Scripts/EnnemySight.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemySight : MonoBehaviour {
+
+	public float fieldOfViewAngle = 110f ;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
+
+	private SphereCollider col;
+	private GameObject player;
+	private LastPlayerSighting lastPlayerSighting;
+	private Vector3 previousSighting;
+
+	void Awake () {
+		col = GetComponent<SphereCollider>();
+		player = GameObject.FindGameObjectWithTag("ChildToy");
+		lastPlayerSighting = GameObject.FindGameObjectWithTag("GameController").GetComponent<LastPlayerSighting>();
+
+		// Set the personal sighting and the previous sighting to the reset position.
+		personalLastSighting = lastPlayerSighting.resetPosition;
+		previousSighting = lastPlayerSighting.resetPosition;
+	}
+
+	void Update () {
+
+		if(lastPlayerSighting.position != previousSighting)
+			personalLastSighting = lastPlayerSighting.position;
+
+		previousSighting = lastPlayerSighting.position;
+	}
+
+	void OnTriggerStay ( Collider other) {
+		// If the player enter the trigger zone
+		if (other.gameObject == player ){
+
+			playerInSight = false;
+
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+
+			//If the angle between forward and where the player is, is less than half the angle of view...
+			if (angle <  fieldOfViewAngle / 0.5f ) {
+
+				RaycastHit hit;
+
+				//... and if a raycast toward the player hits something
+				if (Physics.Raycast(transform.position, direction.normalized, out hit, col.radius)) {
+
+					if ( hit.collider.gameObject == player ) {
+
+						playerInSight = true;
+						lastPlayerSighting.position = player.transform.position;
+						//TODO : personnalLastPlayerPos
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit (Collider other ) {
+		// If the player leaves the trigger zone
+		if (other.gameObject == player ) {
+			playerInSight = false;
+		}
+	
+	}
+
+
+
+}
