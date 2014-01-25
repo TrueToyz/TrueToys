fd6a9fe9d8687d80e69c0564f7bb53e2185ed0b2
Kevin Wagrez <kevin.wagrez@gmail.com>
2014-01-25 01:39:06 +0100

    Simple scene with beginning of grab/drop behaviour for child.
    Signed-off-by: Kevin Wagrez <kevin.wagrez@gmail.com>

--- TrueToyzUnity/Assets/Scripts/ChildBehaviour.cs
+++ TrueToyzUnity/Assets/Scripts/ChildBehaviour.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class ChildBehaviour : MonoBehaviour {
 
+	/* Child-related Variables */
 	public GameObject m_ToyInHand; // Toy actually hold in hand
+	private GameObject m_ChildHand; // The child left hand, recognizable by his tag
+
+	/* Environment related variables */
+	public GameObject Environment;
+
+	/* Switch the environment */
+	public delegate void WorldChanger();
+	public static event WorldChanger childToToy;
+	public static event WorldChanger toyToChild;
 	
 	// Use this for initialization
 	public void Start () {
-	
+		m_ChildHand = GameObject.FindGameObjectWithTag("ChildHand");
 	}
 	
 	// Update is called once per frame
 	public void Update () {
-	
+		TestInteraction();
 	}
 
 	/* ------------------------------------ Control functions --------------------------- */
@@ -22,7 +32,7 @@ public class ChildBehaviour : MonoBehaviour {
 	 * */
 	void TakeControl ()
 	{
-
+		toyToChild(); // Launch all callbacks
 	}
 
 	/*
@@ -32,11 +42,41 @@ public class ChildBehaviour : MonoBehaviour {
 	 * */
 	void ReleaseControl () 
 	{
-
+		childToToy();
 	}
 
 	/* --------------------------------------- Interaction functions --------------------------- */
 
+
+	/*
+	 * Grab the given toy, if close enough
+	 * */
+	void Grab (GameObject toy)
+	{
+		m_ToyInHand = toy;
+		m_ToyInHand.transform.parent = m_ChildHand.transform; // Change toy parent in hierarchy by the hand transform
+		m_ToyInHand.transform.localPosition = Vector3.zero; // Optional : put the object in hand
+
+		// Make it kinematic
+		Rigidbody toyBody = m_ToyInHand.GetComponent<Rigidbody>();
+		toyBody.isKinematic = true;
+	}
+
+	/*
+	 * Release the toy
+	 * */
+	void Drop ()
+	{
+		m_ToyInHand.transform.parent = null; // Toy is not in hierarchy
+		m_ToyInHand.transform.position = m_ChildHand.transform.position;
+
+		// Make it physic
+		Rigidbody toyBody = m_ToyInHand.GetComponent<Rigidbody>();
+		toyBody.isKinematic = false;
+
+		// Erase pointer
+		m_ToyInHand = null;
+	}
 
 	/* ------------------------------------------ Menu functions ---------------------------------- */
 
@@ -46,5 +86,25 @@ public class ChildBehaviour : MonoBehaviour {
 	void Pause ()
 	{
 
+	}
+
+	/* --------------------------------------------- Debug functions --------------------------------- */
+
+	/*
+	 * Test if behavior of child is good
+	 * */
+	void TestInteraction ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (!m_ToyInHand)
+			{
+				GameObject ChildToy = GameObject.FindGameObjectWithTag("ChildToy");
+				Grab (ChildToy);
+			}
+			else{
+				Drop ();
+			}
+		}
 	}
 }
