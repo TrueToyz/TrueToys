96cf62e4b056a424f2790c0f9668dbe7029b1720
Kevin Wagrez <kevin.wagrez@gmail.com>
2014-01-26 08:17:53 +0100

    Effect manager can change from on to off every special effects scripts
    Signed-off-by: Kevin Wagrez <kevin.wagrez@gmail.com>

---
+++ TrueToyzUnity/Assets/Scripts/EffectManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectManager : MonoBehaviour {
+
+	public static GameObject[] ml_SpecialEffects;
+
+	void Start ()
+	{
+		ChildBehaviour.childToToy += ActivateEffects;
+		ChildBehaviour.toyToChild += TerminateEffects;
+
+		ml_SpecialEffects = GameObject.FindGameObjectsWithTag("Effects");
+	}
+
+	public static void ActivateEffects ()
+	{
+
+		// Activate ALL scripts
+		foreach (GameObject effect in EffectManager.ml_SpecialEffects)
+		{
+			effect.SetActive(true);
+			MonoBehaviour[] scriptComponents = effect.GetComponents<MonoBehaviour>();
+			foreach(MonoBehaviour script in scriptComponents) {
+				script.enabled = true;
+			}
+		}
+	}
+
+	public static void TerminateEffects ()
+	{
+		// Activate ALL scripts
+		foreach (GameObject effect in EffectManager.ml_SpecialEffects)
+		{
+			effect.SetActive(false);
+			/*
+			MonoBehaviour[] scriptComponents = effect.GetComponents<MonoBehaviour>();
+			foreach(MonoBehaviour script in scriptComponents) {
+				script.enabled = false;
+			}
+			*/
+		}
+	}
+}
